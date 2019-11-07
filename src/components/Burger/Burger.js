d1723a1040e0abdf8ce8332c5cd8256edda83805
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-11-07 19:16:00 +0530

    generic medhtod for tranforming ingredient object to ingredient array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,21 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredinet';
 
 const burger = (props)=> {
+
     {/*<div className={classes.Burger}>*/}
+    //tranforming an object of key value pair into an array of burger ingredients
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey =>{
+                return [...Array(props.ingredients[igKey])].map((_,i)=>{
+                   return <BurgerIngredient key={igKey+i} type={igKey} />;
+                });
+            });
   return (
       <div className="Burger">
           <BurgerIngredient type="bread-top"/>
-          <BurgerIngredient type="cheese"/>
-          <BurgerIngredient type="wheat"/>
+          {/*<BurgerIngredient type="cheese"/>*/}
+          {/*<BurgerIngredient type="wheat"/>*/}
+          {transformedIngredients}
           <BurgerIngredient type="bread-bottom"/>
 
       </div>

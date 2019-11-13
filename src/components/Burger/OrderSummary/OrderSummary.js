7c1b7a17bd4a5c773aa4a298119e3aff8f8913ac
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-11-13 17:22:07 +0530

    added continue/ cancel button via component and added css

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Auxilary';
+import Button from '../../UI/Button/Button';
 
 const OrderSummary = (props)=> {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -20,7 +21,8 @@ const OrderSummary = (props)=> {
                 {ingredientSummary}
                 <p>Continue to Checkout?</p>
             </ul>
-
+            <Button btnType="Danger" clicked={props.purchaseCancelled} >CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
         </Aux>
 
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,6 +80,9 @@ class BurgerBuilder extends Component {
     purchaseCancelHandler = () => {
         this.setState({purchasing:false});
     }
+    purchaseContinueHandler = () => {
+        alert("You continued");
+    }
 
 // <div>Build Controls i.e. add/remove ingrediants</div>
     render(){
@@ -95,7 +98,10 @@ class BurgerBuilder extends Component {
         return(
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
-                    <OrderSummary ingredients = {this.state.ingredients} />
+                    <OrderSummary
+                        purchaseCancelled = {this.purchaseCancelHandler}
+                        purchaseContinued = {this.purchaseContinueHandler}
+                        ingredients = {this.state.ingredients} />
                 </Modal>>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls

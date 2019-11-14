5e1c9d2a23cf5481f0071e9960d6f66d35faefb6
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-11-15 00:22:11 +0530

    logo image, css and js added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Toolbar.css';
 // import NavigationItems from '../NavigationItems/NavigationItems';
 // import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
-// import Logo from '../../Logo/Logo';
+import Logo from '../../Logo/Logo';
 
 
 const toolbar = (props) => (
     <header className="Toolbar">
         <div>Menu</div>
-        <div>Logo</div>
+        <Logo />
         <nav>
             ...
         </nav>

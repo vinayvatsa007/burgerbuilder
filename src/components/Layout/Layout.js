9d3849e3833bee788b8dc42ec6d39c6f59231781
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-11-16 00:34:40 +0530

    side drawer component and css added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Auxilary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const  Layout = (props)=> (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         {/*<div > Toolbar, Sidebar, Backdrop</div>*/}
         <main className = "Content">
             {props.children}
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const sideDrawer = (props) => {
+    //...
+    return (
+        <div className="SideDrawer">
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;

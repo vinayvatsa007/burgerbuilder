db714c4f611d5a44553f793fdcb99f85ec5f7d90
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-11-16 06:09:23 +0530

    backdrop handled dynamically based on side drawer open/closed using css

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,11 +5,14 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Auxilary';
 const sideDrawer = (props) => {
-    //...
+    let attachedClasses = ["SideDrawer","Close"];
+    if(props.open){
+        attachedClasses = ["SideDrawer","Open"];
+    }
     return (
     <Aux>
         <Backdrop show={props.open} clicked={props.closed} />
-        <div className="SideDrawer">
+        <div className={attachedClasses.join(' ')}>
             <Logo height="11%"/>
             <nav>
                 <NavigationItems/>

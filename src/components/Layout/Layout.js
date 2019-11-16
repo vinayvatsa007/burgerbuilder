d4ce00d063bca8515d4ef2fec98e7095ed7b5bd5
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-11-16 06:00:26 +0530

    backdrop added to side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Aux from '../../hoc/Auxilary';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const  Layout = (props)=> (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        {/*<div > Toolbar, Sidebar, Backdrop</div>*/}
-        <main className = "Content">
-            {props.children}
-        </main>
-    </Aux>
-);
+class  Layout extends Component {
+    state ={
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () =>{
+this.setState({showSideDrawer:false});
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer
+                    open = {this.state.showSideDrawer}
+                    closed = {this.sideDrawerClosedHandler} />
+                <main className = "Content">
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+// const  Layout = (props)=> (
+//
+// );
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxilary';
 const sideDrawer = (props) => {
     //...
     return (
+    <Aux>
+        <Backdrop show={props.open} clicked={props.closed} />
         <div className="SideDrawer">
             <Logo height="11%"/>
             <nav>
                 <NavigationItems/>
             </nav>
         </div>
+    </Aux>
     );
 };
 

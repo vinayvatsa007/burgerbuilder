d5f4814bc574efe26cdbf3119194c351a2801ae2
vinayvatsa007 <vinay.vatsa.007@gmail.com>
2019-11-17 20:03:15 +0530

    modal and OrderSummary converted to classs comp and shouldComponent added to modal to avoid un-necessary rendering of modal on less/more button click

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Auxilary';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props)=> {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
+class OrderSummary extends Component {
+    componentWillUpdate(){
+        console.log('[OrderSummary] will update');
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
                     <span style={{textTransform: 'capitalize'}}>
                         {igKey}
                     </span>
-                    {props.ingredients[igKey]}
-                </li>);
-        });
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-                <p><strong>Total Price:{props.price}</strong></p>
-                <p>Continue to Checkout?</p>
-            </ul>
-            <Button btnType="Danger" clicked={props.purchaseCancelled} >CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
-        </Aux>
-
-);
+                        {this.props.ingredients[igKey]}
+                    </li>);
+            });
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                    <p><strong>Total Price:{this.props.price}</strong></p>
+                    <p>Continue to Checkout?</p>
+                </ul>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled} >CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued} >CONTINUE</Button>
+            </Aux>
+        );
+    }
 }
 export default OrderSummary;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,6 +3,7 @@ import './Toolbar.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import Logo from '../../Logo/Logo';
+import '../../Logo/Logo.css';
 
 
 const toolbar = (props) => (
@@ -12,7 +13,7 @@ const toolbar = (props) => (
         {/*<div onClick={props.drawerToggleClicked}>Menu</div>*/}
         {/*<Logo height="80%"/>*/}
         <div className="Logo">
-            <Logo/>
+            <Logo />
         </div>
         <nav className="DesktopOnly">
             <NavigationItems/>
